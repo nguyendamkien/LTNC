b4758f4fb091e7372b1f0e03aa1a10ce78faea41
Nguyen Dam Kien <kientrungx10st@gmail.com>
2023-02-26 18:16:55 +0700

    Create bai22.cpp

---
+++ week1/bai22.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double weight, height;
+     cin >> weight >> height;
+     cout << setprecision(2) << fixed << weight/(height*height);
+    return 0;
+}
