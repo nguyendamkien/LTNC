88c65a8490978d6678f7d66484f358ed75fb3129
nguyendamkien <kientrungx10st@gmail.com>
2023-02-26 22:35:37 +0700

    Create bai7phanc.cpp

---
+++ week2/bai7phanc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a1, b1, c1, a2, b2, c2;
+
+    cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
+    int det = a1*b2 - a2*b1;
+    if(det == 0)
+    {
+        if(b1*c2 == b2*c1) cout << "Vo so nghiem";
+        else cout << "Vo nghiem";
+    }
+    else
+    {
+        int det1 = c1*b2 - c2*b1;
+        int det2 = a1*c2 - a2*c1;
+        cout << "x = " << det1/det << ", y = " << det2/det;
+    }
+    return 0;
+}
