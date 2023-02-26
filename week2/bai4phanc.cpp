2218c77a3a79aaefcda93f0766a67914030a46ad
nguyendamkien <kientrungx10st@gmail.com>
2023-02-26 22:35:10 +0700

    Create bai4phanc.cpp

---
+++ week2/bai4phanc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int uocchung(int x, int y)
+{
+    while(x*y!=0)
+    {
+        if(x>y) x=x%y;
+        else y=y%x;
+    }
+    return x+y;
+}
+int main()
+{
+    int x, y; cin >> x >> y;
+    int b = uocchung(x, y);
+    cout << x*y/b;
+
+    return 0;
+}
