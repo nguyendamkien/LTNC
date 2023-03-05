93a3654c3d7e725ced7f5f66add8d70ac46d742d
nguyendamkien <kientrungx10st@gmail.com>
2023-03-06 00:03:39 +0700

    Create bai5.cpp

---
+++ week4/BT5/bai5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+bool prime(int n){
+    for(int i=2; i<n; i++)
+        if(n%i==0) return false;
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 2; i <= n; i++)
+        if(prime(i)) cout << i << " ";
+    return 0;
+}
