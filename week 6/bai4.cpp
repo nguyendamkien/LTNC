ba025c1f7502da2d9ec36b6fc6fadff21a97328e
nguyendamkien <kientrungx10st@gmail.com>
2023-03-21 19:47:10 +0700

    Create bai4.cpp

---
+++ week 6/bai4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struct point
+{
+    int x;
+    int y;
+    point(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+
+};
+
+void print(point a)
+{
+    cout << &a.x << ", " << &a.y << endl;
+    cout << &a << endl;
+}
+
+
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    point a(m,n);
+    print(a);
+    \
+    return 0;
+}
