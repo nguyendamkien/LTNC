914e5f96f00eb0d707475d006af210d26226bf2a
nguyendamkien <kientrungx10st@gmail.com>
2023-03-29 00:01:03 +0700

    Create bai1.cpp

---
+++ week7/BT08/bai1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void f(int xval)
+{
+   int x;
+   x = xval;
+   cout << &x << endl;
+}
+void g(int yval)
+{
+   int y;
+   cout << &y << endl;
+}
+int main()
+{
+   f(7);
+   g(11);
+   return 0;
+}
+// trung nhau do deu la bien dia phuong
+
